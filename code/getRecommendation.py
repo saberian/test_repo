441ae6794764b978bdc032c3730c219ee0db5eac
ehsan <mj.saberian@gmail.com>
2015-03-31 21:02:18 -0700

    updated recommendation code, now it is aggrigated over 6 days

--- code/getRecommendation.py
+++ code/getRecommendation.py
@@ -10,41 +10,90 @@ import sys
 from constants import *
 
 
+
+def getRecommForDay(day_str, clfr):
+    print "processing  " + day_str
+    f_name = "day_sample_" + day_str
+    file_loc = PROCESSED_DATA_DIR  + "/"+ f_name + ".pkl"
+    if not os.path.isfile(file_loc):
+        print "get data for " + day_str
+        os.system('python getDaySamples.py ' + day_str)
+    day_data, day_sym_list = pickle.load(open(file_loc, "rb"))
+    goodness_score = clf.decision_function(day_data)
+    ind = np.argsort(-goodness_score)
+    cnt = 0
+    i = 0
+    sym_list = []
+    while (cnt < n_recomms) and (i < len(ind)):
+        t = ind[i]
+        sym = day_sym_list[t]
+        #print sym
+        full_name =  HISTORIC_DATA_LOC + '/' + sym +'.pkl'
+        dl = pickle.load(open(full_name, "rb"))
+        if 'Close' in dl[0]:
+            current_price = float(dl[0]['Close'])
+            current_volume = float(dl[0]['Volume'])/ MILL
+        else:
+            current_price = float(dl[0]['col6'])
+            current_volume = float(dl[0]['col5'])/ MILL
+        if current_price < max_price and current_price > min_price and current_volume > min_vol:
+            sym_list.append(sym)
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            cnt = cnt + 1
+        i = i + 1
+    sys.stdout.write('\n')
+    return sym_list
+
+
+
 max_price = 10
 min_price = 1
 min_vol = 1  # in millions
-n_recomms = 20
+n_recomms = 30
+n_days_thr = 0.6
+n_days = 6
 
-data_date = sys.argv[1]
+target_day = sys.argv[1]
 clf_name =  sys.argv[2]
-
-
-
-print "++++++++++++++++++++++++++++++++++++++++++"
-print "get recommendations for " + data_date
-
-
-res_loc = RECOMMS_DIR + "/" +data_date;
-if not os.path.isdir(res_loc):
-    os.mkdir(res_loc)
-
-f_out = open(res_loc + "/recomms.txt", "w")
-
-f_name = "day_sample_" + data_date
-file_loc = PROCESSED_DATA_DIR  + "/"+ f_name + ".pkl"
-if not os.path.isfile(file_loc):
-    os.system('python getDaySamples.py ' + data_date)
-
-today_data, data_sym_list = pickle.load(open(file_loc, "rb"))
 
 clf = pickle.load(open(MODELS_DIR + "/" + clf_name +"/model.pkl", "rb"))
 
-#goodness_score = clf.predict_proba(today_data)[:,1]
-goodness_score = clf.decision_function(today_data)
 
+print "++++++++++++++++++++++++++++++++++++++++++"
+print "get recommendations for " + target_day
 
-ind = np.argsort(-goodness_score)
-#print goodness_score[ind]
+working_day_list = srl.getWorkingDayList()
+day_ind = working_day_list.index(target_day)
+
+res_hist = {}
+for j in xrange(0, n_days):
+    temp_day = working_day_list[day_ind-j]
+    temp_list = getRecommForDay(temp_day, clf)
+    for sym in temp_list:
+        if sym in res_hist:
+            res_hist[sym] +=1
+        else:
+            res_hist[sym] = 1
+
+sorted_res = sorted(res_hist.items(), key=lambda x:-x[1])
+
+res_loc = RECOMMS_DIR + "/" + target_day;
+if not os.path.isdir(res_loc):
+    os.mkdir(res_loc)
+#f_out = open(res_loc + "/recomms.txt", "w")
+
+for sym, h in sorted_res:
+    if h > n_days*n_days_thr:
+        print sym
+        srl.generateReports(sym, res_loc, h)
+
+log_str = "recomms for " + target_day + " is created"
+print log_str
+srl.writeLogSummary(log_str)
+
+'''
+
 f_out.write("=======Best==============="+"\n")
 cnt = 0
 i = 0
@@ -55,7 +104,7 @@ while (cnt < n_recomms) and (i < len(ind)):
     dl = pickle.load(open(full_name, "rb"))
     current_price = float(dl[0]['Close'])
     current_volume = float(dl[0]['Volume'])/ MILL
-    if current_price < 10 and current_price > 1 and current_volume > 1:
+    if current_price < 1000 and current_price > 1 and current_volume > 1:
         srl.generateReports(sym, res_loc, goodness_score[t])
         log_str = "stock: " + sym + (" score: %.3f" % goodness_score[t]) + \
              " cu price: " + str(current_price) +\
@@ -68,5 +117,5 @@ while (cnt < n_recomms) and (i < len(ind)):
 
 log_str = "recomms based on " + str(today_data.shape[0]) + " stocks for " + data_date + " is created"
 print log_str
-srl.writeLogSummary(log_str)
+srl.writeLogSummary(log_str)'''
 
